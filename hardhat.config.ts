ba0e0894b3c1775875804dcdaed28f260df596b9
Ben Hooper <ben@labrys.io>
2023-06-14 21:42:21 +1000

    feat: 😎 added basic foundry support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,13 @@
 import * as dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig, subtask, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "hardhat-deploy";
+import "@nomicfoundation/hardhat-foundry";
 
 /// TASKS
 import "./tasks/create-circuit-contract";
@@ -23,11 +24,40 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+// This is a task that ignores the foundry test files when compiling in hardhat
+const {
+  TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS,
+} = require("hardhat/builtin-tasks/task-names");
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
+  async (_, __, runSuper) => {
+    const paths = await runSuper();
+
+    return paths.filter((p: string) => !p.endsWith(".t.sol"));
+  }
+);
+
+const importToml = require("import-toml");
+const foundryConfig = importToml.sync("foundry.toml");
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    version: foundryConfig.profile.default.solc_version,
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: foundryConfig.profile.default.optimizer_runs,
+      },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.8.19/metadata.html
+        bytecodeHash: "none",
+      },
+    },
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
